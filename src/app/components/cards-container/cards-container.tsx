53b7921845286a84f9e6b430824e050594fa92bc
Fabricio-Antonio <fabricio.ss2117@gmail.com>
2026-03-08 22:15:45 -0300

    :lipstick: feat: updating card section

--- src/app/components/cards-container/cards-container.tsx
+++ src/app/components/cards-container/cards-container.tsx
@@ -1,38 +1,61 @@
 import { Card } from '@/app/components/card/card'
-import  Icon  from "@/app/components/icon"
+import Icon from "@/app/components/icon"
 import styles from "./cards-container.module.css"
 export default function Cards() {
     return (
-        <>
+        <div className={styles.container}>
             <div className={styles.started}>
                 <div className={styles.get}>
                     <h1>Get</h1>
                 </div>
                 <h1>started</h1>
             </div>
-        <div className={styles.cards}>
-            <Card
-                title="Handbrook"
-                href="#"
-                icon={<Icon icon='material-symbols:book-outline-rounded' width={60} height={60} />}
-            >
-                Learn the language <Icon icon='maki:arrow'></Icon>
-            </Card>
-            <Card
-                title="Playground"
-                href="#"
-                icon={<Icon icon='fe:terminal' width={60} height={60} />}
-            >
-                Try in your browser <Icon icon='maki:arrow'></Icon>
-            </Card>
-            <Card
-                title="Download"
-                href="#"
-                icon={<Icon icon='material-symbols:download' width={60} height={60} />}
-            >
-                Install Slynx <Icon icon='maki:arrow'></Icon>
-            </Card> 
+            <div className={styles.cards}>
+                <Card
+                    title="Handbrook"
+                    href="#"
+                    icon={
+                        <Icon
+                            icon="material-symbols:book-outline-rounded"
+                            width={120}
+                            height={120}
+                            color="var(--card-icons)"
+                        />
+                    }
+                >
+                    Learn the language <Icon icon="maki:arrow" />
+                </Card>
+
+                <Card
+                    title="Playground"
+                    href="#"
+                    icon={
+                        <Icon
+                            icon="fe:terminal"
+                            width={120}
+                            height={120}
+                            color="var(--card-icons)"
+                        />
+                    }
+                >
+                    Try in your browser <Icon icon="maki:arrow" />
+                </Card>
+
+                <Card
+                    title="Download"
+                    href="#"
+                    icon={
+                        <Icon
+                            icon="material-symbols:download"
+                            width={120}
+                            height={120}
+                            color="var(--card-icons)"
+                        />
+                    }
+                >
+                    Install Slynx <Icon icon="maki:arrow" />
+                </Card>
+            </div>
         </div>
-        </>
     )
 }
